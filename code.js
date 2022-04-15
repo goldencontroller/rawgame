01705f3c7e10030352671d961915624fc9fea67a
yikuansun <yikuansun2015@gmail.com>
2022-04-15 12:58:45 -0400

    Get rooms data

--- code.js
+++ code.js
@@ -12,14 +12,21 @@ prompt.addEventListener("blur", function() {
     this.focus();
 });
 
+var request = new XMLHttpRequest();
+request.open('GET', "roomsdata.json", false);
+request.send();
+var rooms = JSON.parse(request.responseText);
+
 var playerData = {
     roomAt: "House",
     inventory: [],
     xp: 0
 }
 function tick(input) {
+    var roomData = rooms[playerData.roomAt];
+    var sights = "You see items: " + roomData.items.join(", ");
     var ctrlhelp = "Now, you can:\n> go [north, south, east, west]\n> get [item]\n> use [item in inventory]";
-    return `\nYour inventory: [${playerData.inventory.join(", ")}] \n${ctrlhelp}\n`;
+    return `\nYou are in the ${playerData.roomAt}\n${sights}\n\nYour inventory: [${playerData.inventory.join(", ")}] \n${ctrlhelp}\n`;
 }
 
 prompt.addEventListener("keydown", function(e) {
